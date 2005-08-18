7d5adba3e57061cf1191808732f7671c0a18dc53
vsasa <vsasa@70ca12d9-96dc-0310-a6d6-b3c0cee7cb29>
2005-08-18 14:12:08 +0000

    git-svn-id: http://repos.sigma-com.net/dev/AF/cl-sclib/sclib@2631 70ca12d9-96dc-0310-a6d6-b3c0cee7cb29

--- commit.sh
+++ commit.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-#echo "Parametri $@"
+echo "---------------------------------------------------"
+echo "== Commit 2 Repos ================================="
+echo "++++++ Ulazni parametri: $1 $2"
 
-CMD=$@
+#define VERSION const
+VERSION=`cat /c/dev/fmk/$1/$1.ch | grep _VERZIJA | awk -F '"' '{ print $2}'`
 
-echo "Parametri: $CMD"
+echo "++++++ Trenutna verzija modula $1 : $VERSION"
+echo ""
+echo "++++++++++++++++++++++"
 
-/c/"Program Files"/Subversion/bin/svn commit -m $CMD 
+/c/"Program Files"/Subversion/bin/svn commit -m $VERSION $2 
+
+echo "++++++++++++++++++++++"
