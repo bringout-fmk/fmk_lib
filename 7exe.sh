9a167f7d57dd7944d5cd5e17b6f7551fc995ab81
vsasa <vsasa@70ca12d9-96dc-0310-a6d6-b3c0cee7cb29>
2005-08-18 14:12:02 +0000

    git-svn-id: http://repos.sigma-com.net/dev/AF/cl-sclib/sclib@2630 70ca12d9-96dc-0310-a6d6-b3c0cee7cb29

--- 7exe.sh
+++ 7exe.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#echo "Parametri $@"
-
+echo "-------------------------------------------"
+echo "== 7zip::ZIP 2 EXE ========================"
 CMD=$@
-
-echo "Parametri: $CMD"
-
+echo "++++ Ulazni parametri: $CMD"
+echo ""
 /c/"Program Files"/"7-Zip"/7z a -sfx7z.sfx $CMD 
+echo "+++++++++++++++++++++"
+
