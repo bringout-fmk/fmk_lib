af22c6141d4038023ff5b37f0a61cd2241813085
Ernad Husremovic <hernad@bring.out.ba>
2012-03-24 15:30:51 +0100

    zastita se BX.XV se ukida

--- dosemu_drive_c/asm52.bat
+++ dosemu_drive_c/asm52.bat
@@ -1,10 +1,10 @@
-rem cd %1
+cd %1
 
 set PATH=%PATH%;C:\clipper\bin
 set INCLUDE=C:\CLIPPER\INCLUDE;c:\git\fmk_lib\;c:\git\fmk_c\cdx;c:\clipper\comix;c:\clipper\csy\include
 set LIB=c:\clipper\lib;c:\clipper\comix;c:\git\fmk_lib\lib;c:\clippper\csy\lib
 
 
-c:\clipper\c5\bin\masm %1 %2 %3 %4 %5 %6 %7 %8 %9 ,,,,
+c:\clipper\c5\bin\masm %2 %3 %4 %5 %6 %7 %8 %9 ,,,,
 
 exitemu
